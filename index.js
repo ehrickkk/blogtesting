880ef2ff0a0d1f638288e43ed97429dff4067748
ehrickkk <lor.eric.436@gmail.com>
2024-05-15 23:07:37 +0800

    uploadingtesting
    hsdsj

---
+++ index.js
@@ -0,0 +1,27 @@
+//IMAGE SLIDER
+
+const slides = document.querySelectorAll(".slides img");
+let slideIndex = 0;
+let intervalId = null;
+
+function initializeSlider()
+{
+    slides[slideIndex].classList.add("displaySlide");
+}
+
+function showSlide()
+{
+
+}
+
+function preSlide()
+{
+
+}
+
+function nextSlide()
+{
+
+}
+
+
